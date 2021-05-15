5aea7209b59f03917d60bd518a9d525e9b156319
Vijay Kumar <vijay.kumar@emvigotech.com>
2021-05-16 00:41:53 +0530

    WIP till 35

--- src/index.js
+++ src/index.js
@@ -7,23 +7,15 @@ import reportWebVitals from './reportWebVitals'
 const rootElement = document.getElementById('root')
 
 function render() {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    rootElement
-  )
+  ReactDOM.render(<App />, rootElement)
 }
 
-if (module.hot) {
-  module.hot.accept('./app/layout/App', function () {
-    setTimeout(render)
-  })
-}
+// if (module.hot) {
+//   module.hot.accept('./app/layout/App', function () {
+//     setTimeout(render)
+//   })
+// }
 
 render()
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
